94d1f7121a7ea81950426daa3ec38e8f318554be
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2025-11-28 18:58:10 +0200

    autocomit pv421

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+import uvicorn
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import BaseModel, EmailStr, Field
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from typing import Optional
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from typing_extensions import deprecated
+
+# Config
+SECRET_KEY = "some-secret-kay-change-on-prod"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Init
+app = FastAPI(title="FastAPi", version="0.0.1")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+fake_users_db = {}
+security = HTTPBearer()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Models
+class UserRegister(BaseModel):
+    username: str = Field(..., min_length=3, max_length=50)
+    email: EmailStr
+    password: str = Field(..., min_length=6)
 
 
-# Press the green button in the gutter to run the script.
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class PasswordChange(BaseModel):
+    current_password: str
+    new_password: str = Field(..., min_length=6)
+
+
+class UserResponse(BaseModel):
+    username: str
+    email: str
+    message: str
+
+
+class HealthResponse(BaseModel):
+    status: int
+    success: bool
+    message: str
+
+
+# default endpoints
+@app.get("/api/health")
+def health():
+    return HealthResponse(
+        success=True,
+        status=200,
+        message="ok!"
+    )
+
+@app.get("/")
+def root():
+    return "Server is working!"
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=3000)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

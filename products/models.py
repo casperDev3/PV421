e124c3673fff11408aefc5093d21792096bfdb86
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2025-12-20 11:49:04 +0200

    [AUTO] add new part code from lesson

--- products/models.py
+++ products/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="Ідентифікатор продукту")
+    # add relation to User model
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Користувач")
+    # product fields
     name = models.CharField(max_length=255, verbose_name="Назва продукту")
     description = models.TextField(verbose_name="Опис продукту")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Ціна продукту")
--- products/serializers.py
+++ products/serializers.py
@@ -5,10 +5,10 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         # Поля, які ми повертаємо через API
-        fields = ['id', 'name', 'description', 'price', 'is_available', 'created_at']
+        fields = ['id', 'user', 'name', 'description', 'price', 'is_available', 'created_at']
 
 class ProductsSidebarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         # Поля для відображення в сайдбарі
-        fields = ['id', 'name', 'price', 'is_available']
+        fields = ['id', 'user', 'name', 'price', 'is_available']

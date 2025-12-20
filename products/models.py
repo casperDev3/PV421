bc72695d64df4004906039e4a1ec7f9b07afcc79
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2025-12-20 13:37:01 +0200

    [AUTO] add new part code from lesson

--- products/models.py
+++ products/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save, post_save, pre_delete, post_delete
+from django.dispatch import receiver
+from sqlalchemy.orm.persistence import post_update
 
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="Ідентифікатор продукту")
     # add relation to User model
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Користувач")
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, verbose_name="Користувач")
     # product fields
     name = models.CharField(max_length=255, verbose_name="Назва продукту")
     description = models.TextField(verbose_name="Опис продукту")
@@ -20,3 +23,34 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукти"
+
+
+# _____ SIGNALS FOR PRODUCT MODEL _____ #
+# BEFORE CREATE
+@receiver(pre_save, sender=Product)
+def before_saving_product(sender, instance, **kwargs):
+    if instance._state.adding:
+        print(f"Перед створенням продукту: {instance.name}")
+    else:
+        print(f"Перед оновленням продукту: {instance.name}")
+
+# AFTER CREATE
+@receiver(post_save, sender=Product)
+def after_saving_product(sender, instance, created, **kwargs):
+    if created:
+        print(f"Продукт створено: {instance.name}")
+    else:
+        print(f"Продукт оновлено: {instance.name}")
+
+# TODO: Переглянути сигнали для оновлення моделі (перед і після оновлення)
+# BEFORE DELETE
+@receiver(pre_delete, sender=Product)
+def before_deleting_product(sender, instance, **kwargs):
+    print(f"Перед видаленням продукту: {instance.name}")
+
+# AFTER  DELETE
+@receiver(post_delete,  sender=User)
+def after_deleting_product(sender, instance, **kwargs):
+    print(f"Продукт видалено: {instance.name}")
+
+
--- products/views.py
+++ products/views.py
@@ -1,14 +1,49 @@
+from django.core.serializers import serialize
 from rest_framework import viewsets, filters
 from .models import Product
 from .serializers import ProductSerializer, ProductsSidebarSerializer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+
+    # permissions for CRUD operations
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Встановлюємо користувача при створенні продукту
+    # BEFORE CREATE / ON CREATE
+    def perform_create(self, serializer):
+        print("ЗАПИТ НА СТВОРЕННЯ ПРОДУКТУ:")
+        serializer.save(user=self.request.user)
+
+    # BEFORE UPDATE / ON UPDATE
+    def perform_update(self, serializer):
+        print("ЗАПИТ НА ОНОВЛЕННЯ ПРОДУКТУ:")
+
+    # BEFORE DELETE / ON DELETE
+    def perform_destroy(self,  instance):
+        print("ЗАПИТ НА ВИДАЛЕННЯ ПРОДУКТУ:")
+
+
+    # Кастомний шлях: отримати дорогі продукти (ціна > 1000) через /api/products/expensive/
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticatedOrReadOnly])
+    def expensive(self, request):
+        expensive_products = Product.objects.filter(price__gt=1000)  # приклад фільтрації дорогих продуктів
+        serializer = self.get_serializer(expensive_products, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['put'], permission_classes=[IsAuthenticated])
+    def toggle(self, request, pk=None):
+        product = self.get_object()
+        product.is_available = not product.is_available
+        product.save()
+        serializer = self.get_serializer(product)
+        return Response(serializer.data)
 
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     # filtering

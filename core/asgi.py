53c1922f745202fabeb7d7bd83f95d823a3b50b4
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-01-14 19:01:52 +0200

    [AUTO] add new part code from lesson

--- core/asgi.py
+++ core/asgi.py
@@ -4,7 +4,6 @@ from channels.routing import  ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from notifications.routing import websocket_urlpatterns
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
--- notifications/consumers.py
+++ notifications/consumers.py
@@ -1,6 +1,12 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+room_state = {
+    "color": "#ffffff",
+    "counter": 0
+}
+
+
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         # name of the room/group
@@ -11,7 +17,6 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
         await self.accept()
-
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -24,4 +29,54 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             'type_notification': event['type_notification'],
             'message': event['message'],
             'sender': str(event['sender'])
+        }))
+
+
+class InteractiveConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = 'interactive_room'
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+        await self.send(text_data=json.dumps({
+            "type": "sync",
+            "state": room_state
+        }))
+        print("Клієнт підключився до інтерактивної кімнати!")
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+        print("Клієнт відключився від інтерактивної кімнати!")
+
+    async def receive(self, text_data=None, bytes_data=None):
+        global room_state
+        data = json.loads(text_data)
+        command = data.get("command")
+
+        if command == "change_color":
+            new_color = data.get("color")
+            room_state["color"] = new_color
+        elif command == "increment":
+            room_state["counter"] += 1
+        elif command == "reset":
+            room_state["counter"] = 0
+
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "broadcast_state",
+                "state": room_state
+            }
+        )
+    async def broadcast_state(self,  event):
+        await self.send(text_data=json.dumps({
+            "type": "update",
+            "state": event["state"]
         }))
